6aca39ef175433affa6e8facbd8067ddbf5f00a2
Illya_Puchkovskiy <illya.puc@incrowdsports.com>
2022-12-16 16:23:26 +0200

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: process.env.NODE_ENV === 'production'
-    ? '/smart-check/'
-    : '/'
+  publicPath: '/smart-check/'
 })
