ddd4b4b7317f02325c709807e480e279f3422b1d
Illya_Puchkovskiy <illya.puc@incrowdsports.com>
2023-01-23 15:38:49 +0200

    add links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Главная страница</h1>
+    <RouterLink to="/noski" class="nav-link">Носки</RouterLink>
+    <RouterLink to="/moloko" class="nav-link">Молоко</RouterLink>
   </div>
 </template>
 
